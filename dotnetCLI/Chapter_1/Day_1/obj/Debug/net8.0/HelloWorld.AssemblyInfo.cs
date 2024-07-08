2625d14b644e45ee4deb2a5ea299ac4c0f143fd3
Christian Solomon <86303900+chris-zano@users.noreply.github.com>
2024-07-08 22:45:19 +0000

    restructure project

--- dotnetCLI/Chapter_1/Day_1/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ dotnetCLI/Chapter_1/Day_1/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b97b43a04724f49cb6f3582d7c33f9dd45b114d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
