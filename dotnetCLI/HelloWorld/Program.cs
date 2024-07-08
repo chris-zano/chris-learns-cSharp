4b97b43a04724f49cb6f3582d7c33f9dd45b114d
Christian Solomon <86303900+chris-zano@users.noreply.github.com>
2024-07-08 22:41:40 +0000

    introduction to c#
    this is my first commit to learning c#
    I learned about the c# language:
    --- how to install the c# compiler
    --- how to create a simple class in c# ( class NameOfClass {} )
    --- how to use dotnet CLI to build and run my c# code

---
+++ dotnetCLI/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No command line arguments were passed.");
+            Console.Write("here is the program name: ");
+            Console.WriteLine(Environment.CommandLine);
+            return 1;
+        }
+        else
+        {
+            int i = 0;
+            foreach (string arg in args)
+            {
+                Console.WriteLine($"argument at index {i}: {arg}");
+                i++;
+            }
+        }
+
+        return 0;
+    }
+}
